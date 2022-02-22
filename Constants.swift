ab13ae39c4509537e6ca6df4def27e63ae110808
Sergey <bindasov.sergey@gmail.com>
2022-02-22 16:06:14 +0300

    Добавил файл с бренд цветами и занес их в ассеты.

---
+++ Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Sergey on 22.02.2022.
+//
+
+import Foundation
+
+struct K {
+    struct BrandColors {
+        static let blue = "brandBlue"
+        static let lightBlue = "lightBlue"
+        static let blackText = "blackText"
+        static let lightText = "lightText"
+        static let orange = "orange"
+        static let green = "green"
+        static let background = "background"
+        static let whiteText = "whiteText"
+        static let yellow = "yellow"
+    }
+}
--- OnboardingViewController.swift
+++ OnboardingViewController.swift
@@ -11,7 +11,7 @@ class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = UIColor(named: K.BrandColors.blue)
     }
 
 
